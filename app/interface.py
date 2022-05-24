1d7872489f87efc9475efa9966598f19509f03f3
iscodand <iscodand@outlook.com>
2022-05-23 23:59:13 -0300

    refactoring code

---
+++ app/interface.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+import sv_ttk
+from tkinter import ttk
+
+
+class PomodoroInterface():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("500x425")
+        self.root.title("Pymodoro Timer")
+        self.root.call('wm', 'iconphoto', self.root._w, tk.PhotoImage(
+            file='app\icon\pymodoro-icon.png'))
+
+        # Interface Layout
+        self.s = ttk.Style()
+        self.s.configure("TNotebook.Tab", padding=6, font=("Unbutu", 12))
+        self.s.configure("TButton", font=("Unbutu", 12))
+
+        sv_ttk.set_theme("dark")
+
+        self.tabs = ttk.Notebook(self.root)
+        self.tabs.pack(fill="both", pady=50, padx=50)
+
+        self.tab1 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab2 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab3 = ttk.Frame(self.tabs, width=600, height=100)
+
+        self.pomodoro_timer_label = ttk.Label(
+            self.tab1, text="25:00", font=("Unbutu", 50))
+        self.pomodoro_timer_label.pack(pady=50)
+
+        self.short_break_timer_label = ttk.Label(
+            self.tab2, text="05:00", font=("Unbutu", 50))
+        self.short_break_timer_label.pack(pady=50)
+
+        self.long_break_timer_label = ttk.Label(
+            self.tab3, text="15:00", font=("Unbutu", 50))
+        self.long_break_timer_label.pack(pady=50)
+
+        self.tabs.add(self.tab1, text="Code Time")
+        self.tabs.add(self.tab2, text="Coffee")
+        self.tabs.add(self.tab3, text="Social")
+
+        self.grid_layout = ttk.Frame(self.root)
+        self.grid_layout.pack()
+
+        self.start_button = ttk.Button(
+            self.grid_layout, text="Start", command=self.start_timer_thread)
+        self.start_button.grid(row=0, column=0, padx=10)
+
+        self.reset_button = ttk.Button(
+            self.grid_layout, text="Reset", command=self.reset_clock)
+        self.reset_button.grid(row=0, column=1, padx=10)
+
+        self.skip_button = ttk.Button(
+            self.grid_layout, text="Skip", command=self.skip_clock)
+        self.skip_button.grid(row=0, column=2, padx=10)
+
+        self.pomodoros_count_label = ttk.Label(
+            self.grid_layout, text="Pomodoros: 0", font=("Ubuntu Bold", 14))
+        self.pomodoros_count_label.grid(row=1, column=1, pady=30)
+
+        self.pomodoros = 0
+        self.stopped = False
+        self.skipped = False
+        self.running = False
+
+        self.root.resizable(False, False)
+        self.root.mainloop()
--- app/main.py
+++ app/main.py
@@ -1,72 +1,10 @@
 import threading
 import time
-import tkinter as tk
-from tkinter import ttk
 from playsound import playsound
+from interface import PomodoroInterface
 
 
-class PomodoroTimer():
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.geometry("500x425")
-        self.root.title("Pymodoro Timer")
-        self.root.call('wm', 'iconphoto', self.root._w, tk.PhotoImage(
-            file='app\icon\pymodoro-icon.png'))
-
-        # Interface Layout
-        self.s = ttk.Style()
-        self.s.configure("TNotebook.Tab", padding=6, font=("Unbutu", 12))
-        self.s.configure("TButton", font=("Unbutu", 12))
-
-        self.tabs = ttk.Notebook(self.root)
-        self.tabs.pack(fill="both", pady=50, padx=50)
-
-        self.tab1 = ttk.Frame(self.tabs, width=600, height=100)
-        self.tab2 = ttk.Frame(self.tabs, width=600, height=100)
-        self.tab3 = ttk.Frame(self.tabs, width=600, height=100)
-
-        self.pomodoro_timer_label = ttk.Label(
-            self.tab1, text="25:00", font=("Ubuntu", 50))
-        self.pomodoro_timer_label.pack(pady=50)
-
-        self.short_break_timer_label = ttk.Label(
-            self.tab2, text="05:00", font=("Ubuntu", 50))
-        self.short_break_timer_label.pack(pady=50)
-
-        self.long_break_timer_label = ttk.Label(
-            self.tab3, text="15:00", font=("Ubuntu", 50))
-        self.long_break_timer_label.pack(pady=50)
-
-        self.tabs.add(self.tab1, text="Pomodoro")
-        self.tabs.add(self.tab2, text="Short Break")
-        self.tabs.add(self.tab3, text="Long Break")
-
-        self.grid_layout = ttk.Frame(self.root)
-        self.grid_layout.pack()
-
-        self.start_button = ttk.Button(
-            self.grid_layout, text="Start", command=self.start_timer_thread)
-        self.start_button.grid(row=0, column=0, padx=10)
-
-        self.reset_button = ttk.Button(
-            self.grid_layout, text="Reset", command=self.reset_clock)
-        self.reset_button.grid(row=0, column=1, padx=10)
-
-        self.skip_button = ttk.Button(
-            self.grid_layout, text="Skip", command=self.skip_clock)
-        self.skip_button.grid(row=0, column=2, padx=10)
-
-        self.pomodoros_count_label = ttk.Label(
-            self.grid_layout, text="Pomodoros: 0", font=("Ubuntu", 12))
-        self.pomodoros_count_label.grid(row=1, column=1, pady=20)
-
-        self.pomodoros = 0
-        self.stopped = False
-        self.skipped = False
-        self.running = False
-
-        self.root.mainloop()
-
+class PomodoroTimer(PomodoroInterface):
     def start_timer_thread(self):
         if not self.running:
             t = threading.Thread(target=self.start_timer)
@@ -80,6 +18,7 @@ class PomodoroTimer():
 
         if timer_id == 1:
             full_seconds = 60 * 25
+            full_seconds = 3
 
             while full_seconds > 0 and not self.stopped:
                 minutes, seconds = divmod(full_seconds, 60)
