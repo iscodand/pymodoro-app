ff0e8cba07de2bb1af72720c52fc8aacd1da48b1
iscodand <iscodand@oulook.com>
2022-05-25 16:32:28 -0300

    refactoring code

--- app/interface.py
+++ app/interface.py
@@ -10,42 +10,52 @@ class PomodoroInterface():
         self.root.geometry("550x550")
         self.root.title("Pymodoro Timer")
         self.root.call('wm', 'iconphoto', self.root._w, tk.PhotoImage(
-            file='app\icon\pymodoro-icon.png'))
+            file='app\icons\pymodoro-icon.png'))
 
-        # Interface Layout
+        # Setting Style
         self.s = ttk.Style()
         self.s.configure("TNotebook.Tab", padding=6, font=("Montserrat", 12))
         self.s.configure("TButton", font=("Montserrat", 12))
 
         sv_ttk.set_theme("dark")
 
+        # Setting Tabs with ttk.Notebook
         self.tabs = ttk.Notebook(self.root, width=150)
-        self.tabs.pack(pady=50, padx=50)
+        self.tabs.pack(fill="both", pady=50, padx=50)
 
         self.tab1 = ttk.Frame(self.tabs)
         self.tab2 = ttk.Frame(self.tabs)
         self.tab3 = ttk.Frame(self.tabs)
 
         self.pomodoro_timer_label = ttk.Label(
-            self.tab1, text="25:00", font=("Montserrat", 50))
+            self.tab1, text="25:00", font=("Montserrat", 60))
         self.pomodoro_timer_label.pack(pady=50)
 
         self.short_break_timer_label = ttk.Label(
-            self.tab2, text="05:00", font=("Montserrat", 50))
+            self.tab2, text="05:00", font=("Montserrat", 60))
         self.short_break_timer_label.pack(pady=50)
 
         self.long_break_timer_label = ttk.Label(
-            self.tab3, text="15:00", font=("Montserrat", 50))
+            self.tab3, text="15:00", font=("Montserrat", 60))
         self.long_break_timer_label.pack(pady=50)
 
         self.pomodoros_count_label = ttk.Label(
-            self.tab1, text="Pomodoro #1", font=("Montserrat Thin", 10))
+            self.tab1, text="Pomodoro #1", font=("Montserrat Thin", 9))
+        self.pomodoros_count_label.pack(pady=20)
+
+        self.pomodoros_count_label = ttk.Label(
+            self.tab2, text="Coffee Time!", font=("Montserrat Thin", 9))
+        self.pomodoros_count_label.pack(pady=20)
+
+        self.pomodoros_count_label = ttk.Label(
+            self.tab3, text="Long Rest Time!", font=("Montserrat Thin", 9))
         self.pomodoros_count_label.pack(pady=20)
 
         self.tabs.add(self.tab1, text="Code Time")
         self.tabs.add(self.tab2, text="Coffee")
         self.tabs.add(self.tab3, text="Social")
 
+        # Set the buttons and his respective positions
         self.grid_layout = ttk.Frame(self.root)
         self.grid_layout.pack()
 
@@ -61,8 +71,9 @@ class PomodoroInterface():
             self.grid_layout, text="Skip", command=self.skip_clock)
         self.skip_button.grid(row=0, column=2, padx=10)
 
-        self.choose_theme_icon = tk.PhotoImage(file="app\icon\light_icon.png")
-        self.choose_theme_icon_sample = self.choose_theme_icon.subsample(10, 10)
+        self.choose_theme_icon = tk.PhotoImage(file="app\icons\light_icon.png")
+        self.choose_theme_icon_sample = self.choose_theme_icon.subsample(
+            10, 10)
 
         self.choose_theme_button = ttk.Button(
             self.grid_layout,
@@ -71,13 +82,14 @@ class PomodoroInterface():
             command=self.choose_theme)
         self.choose_theme_button.grid(row=2, column=1, pady=30)
 
+        # Set the fundamentals to PomodoroTimer Class
         self.pomodoros = 1
         self.stopped = False
         self.skipped = False
         self.running = False
 
-        self.root.resizable(False, False)
         self.root.mainloop()
+        self.root.resizable(False, False)
 
     def choose_theme(self):
         time.sleep(0.25)
---
+++ app/timer.py
@@ -0,0 +1,102 @@
+import threading
+import time
+import winsound
+from interface import PomodoroInterface
+
+class PomodoroTimer(PomodoroInterface):
+    def start_timer_thread(self):
+        if not self.running:
+            t = threading.Thread(target=self.start_timer)
+            t.start()
+            self.running = True
+
+    def start_timer(self):
+        self.stopped = False
+        self.skipped = False
+        timer_id = self.tabs.index(self.tabs.select()) + 1
+
+        if timer_id == 1:
+            full_seconds = 60 * 25
+
+            while full_seconds > 0 and not self.stopped:
+                minutes, seconds = divmod(full_seconds, 60)
+                self.pomodoro_timer_label.config(
+                    text=f"{minutes:02d}:{seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                full_seconds -= 1
+
+            if not self.stopped or self.skipped:
+                winsound.PlaySound(sound="app\sounds\sound.wav",
+                                   flags=winsound.SND_FILENAME)
+                self.pomodoros += 1
+                self.pomodoros_count_label.config(
+                    text=f"Pomodoro #{self.pomodoros}")
+
+                if self.pomodoros % 4 == 0:
+                    self.tabs.select(2)
+
+                else:
+                    self.tabs.select(1)
+
+                self.start_timer()
+
+        elif timer_id == 2:
+            full_seconds = 60 * 5
+
+            while full_seconds > 0 and not self.stopped:
+                minutes, seconds = divmod(full_seconds, 60)
+                self.short_break_timer_label.config(
+                    text=f"{minutes:02d}:{seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                full_seconds -= 1
+
+            if not self.stopped or self.skipped:
+                winsound.PlaySound(sound="app\sounds\sound.wav",
+                                    flags=winsound.SND_FILENAME)
+                self.tabs.select(0)
+                self.start_timer()
+
+        elif timer_id == 3:
+            full_seconds = 60 * 15
+
+            while full_seconds > 0 and not self.stopped:
+                minutes, seconds = divmod(full_seconds, 60)
+                self.long_break_timer_label.config(
+                    text=f"{minutes:02d}:{seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                full_seconds -= 1
+
+            if not self.stopped or self.skipped:
+                winsound.PlaySound(sound="app\sounds\sound.wav",
+                                   flags=winsound.SND_FILENAME)
+                self.tabs.select(0)
+                self.start_timer()
+
+        else:
+            print('Invalid Timer ID!')
+
+    def reset_clock(self):
+        self.stopped = True
+        self.skipped = False
+        self.running = False
+        self.pomodoros = 0
+        self.pomodoro_timer_label.config(text="25:00")
+        self.short_break_timer_label.config(text="05:00")
+        self.long_break_timer_label.config(text="15:00")
+        self.pomodoros_count_label.config(text="Pomodoro #1")
+
+    def skip_clock(self):
+        current_tab = self.tabs.index(self.tabs.select())
+
+        if current_tab == 0:
+            self.pomodoro_timer_label.config(text="25:00")
+        elif current_tab == 1:
+            self.short_break_timer_label.config(text="05:00")
+        elif current_tab == 2:
+            self.long_break_timer_label.config(text="15:00")
+
+        self.skipped = True
+        self.stopped = True
