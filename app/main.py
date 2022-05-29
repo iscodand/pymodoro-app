5fce5106dd28adcf8b37179430eeaf0b65fd4291
iscodand <iscodand@outlook.com>
2022-05-29 15:04:21 -0300

    organizing folders

--- app/main.py
+++
@@ -1,4 +0,0 @@
-from timer import PymodoroTimer
-
-if __name__ == '__main__':
-    PymodoroTimer()
---
+++ app/scr/main.py
@@ -0,0 +1,8 @@
+from timer import PymodoroTimer
+from interface import PymodoroInterface
+from notifications import PymodoroNotifications
+
+if __name__ == '__main__':
+    PymodoroTimer()
+    PymodoroInterface()
+    PymodoroNotifications()
